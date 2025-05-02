75fc4bd9d2be36b5bd45943448937a6f20ead96c
Koda-Shigekuni-10514 <600-student005@la-study.com>
2025-05-02 16:57:42 +0900

    SP40:再プッシュ

---
+++ spring-chap5-parameter-session/src/main/java/com/example/demo/SpringChap5ParameterSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChap5ParameterSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChap5ParameterSessionApplication.class, args);
+	}
+
+}
