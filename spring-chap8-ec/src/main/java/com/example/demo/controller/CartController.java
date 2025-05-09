0104163969092dbe666fd162628f04f0acc1e869
Koda-Shigekuni-10514 <600-student005@la-study.com>
2025-05-09 17:30:13 +0900

    商品追加まで完了

---
+++ spring-chap8-ec/src/main/java/com/example/demo/controller/CartController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Item;
+import com.example.demo.model.Cart;
+import com.example.demo.repository.ItemRepository;
+
+@Controller
+public class CartController {
+
+
+    @Autowired
+    Cart cart;
+    
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @GetMapping("/cart")
+    public String index(){
+        return "cart";
+    }
+
+    @PostMapping("/cart/add")
+    public String addCart(
+        @RequestParam(name="itemId") Integer itemId
+    ){  
+        
+
+    	Optional<Item> dbData = itemRepository.findById(itemId);
+
+        if(!dbData.isEmpty()){
+            cart.add(dbData.get());
+        }
+        
+        return "redirect:/cart";
+    }
+
+}
